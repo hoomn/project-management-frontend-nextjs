6c56627d72ac2598ee3a47831bf65f3065d678c2
Hooman G <hooman_g@live.com>
2024-10-19 14:10:42 -0700

    update layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 
+import Footer from "@/components/navigation/footer";
+import Header from "@/components/navigation/header";
+import Container from "react-bootstrap/Container";
+
 import Providers from "./providers";
 
 import "./globals.css";
@@ -17,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="">
-        <Providers>{children}</Providers>
+        <Header />
+        <Container className="my-5" style={{ minHeight: "calc(100vh - 256px)" }}>
+          <Providers>{children}</Providers>
+        </Container>
+        <Footer />
       </body>
     </html>
   );
